3aa34bee47be191e4a83eefda8d27d7f63e4af5b
csw <angelso2002@gmail.com>
2017-11-13 21:35:51 +0900

    everything_all

--- barcode.py
+++ barcode.py
@@ -2,9 +2,5 @@
 
 while True:
     barcode = input("Scan barcode: ")
-<<<<<<< HEAD
+
     print "Barcode scanned asdfaddf: " + barcode
-=======
-    print "Barcode scanned: ahnjunyoung modify" + barcode
-    print "Barcode scanned asdfa: " + barcode
->>>>>>> 4049feed560331fe98d0fab2d7397ee0c7b250bb
