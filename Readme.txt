eaa198bb169ad8ed005cba999c1bad9a8bb19fc6
ljs <dlwwwwjdtlr@naver.com>
2017-11-22 00:28:35 +0900

    Test code for connecting to mysql_db_server

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,2 @@
-29/10/2017 17:48 The Readme file was initialized.29/10/2017 17:48 The Readme file was initialized.
+29/10/2017 17:48 The Readme file was initialized.
+22/11/2017 00:27 Pushed Test code for connecting to mysql_db_server.
--- barcode.py
+++ barcode.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
+import pymysql
 
+#DB connection
+conn = pymysql.connect(host='ajy.iptime.org', port=3306, user='ajy0714', password='940714a', db='capstone')
+
+#Download DB from server to RaspberryPi
+cur=conn.cursor(pymysql.cursors.DictCursor)
+
+#SQL query that will be executed
+sql_mat="select prod_mat from recycleBin where barcode=%s"
+
+#Get data from barcode scanner and determine the 
 while True:
     barcode = input("Scan barcode: ")
+    cur.execute(sql_mat, (barcode))
+    res_material=cur.fetchall()
 
-    print "Barcode scanned asdfaddf: " + barcode
+    #get data in dictionary type from list(named res_material)
+    for row in res_material:
+	    print(row['prod_mat'])
+
+
+#Close DB connection
+conn.close()
+
